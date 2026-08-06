02573dfa53ff6b2b78fe07869c27f549fcd47f5f
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-06 21:13:35 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
         int n=nums.length;
         for(int i=0 ; i<n ; i++){
-            map.put(nums[i],1);
+            sum+=nums[i];
         }
-        for(int i=0 ; i<=n ; i++){
-            if(!map.containsKey(i)) return i;
-        }
-        return 0;
-        
-        // int sum=0;
+        int cal=n*(n+1)/2;
+        return cal-sum;
+        // HashMap<Integer,Integer> map=new HashMap<>();
         // int n=nums.length;
         // for(int i=0 ; i<n ; i++){
-        //     sum+=nums[i];
+        //     map.put(nums[i],1);
         // }
-        // int cal=n*(n+1)/2;
-        // return cal-sum;
+        // for(int i=0 ; i<=n ; i++){
+        //     if(!map.containsKey(i)) return i;
+        // }
+        // return 0;
     }
 }
