440337fbb42cff8ccb4955e669222f7e29801fa2
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-10 01:12:55 +0530

    Time: 1 ms (100%), Space: 46.8 MB (49.13%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -2,18 +2,45 @@ class Solution {
     public int[] countOppositeParity(int[] nums) {
         int n=nums.length;
         int[] ans=new int[n];
-        for(int i=0 ;i<n ; i++){
-            int count=0;
-            for(int j=i+1 ; j<n ; j++){
-                if(nums[i]%2==0){
-                    if(nums[j]%2!=0) count++;
+        int oddcount=0;
+        int evencount=0;
+        if(n==1) return new int[]{0};
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]%2==0) evencount++;
+            else oddcount++;
+        }
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]%2==0){
+                ans[i]=oddcount;
+                if(evencount!=0){
+                    evencount--;
                 }
-                else{
-                    if(nums[j]%2==0) count++;
-                }
+               
             }
-            ans[i]=count;
+            else{
+                ans[i]=evencount;
+                if(oddcount!=0){
+                    oddcount--;
+                }
+                
+            }
         }
         return ans;
+        
+        
+        
+        // for(int i=0 ;i<n ; i++){
+        //     int count=0;
+        //     for(int j=i+1 ; j<n ; j++){
+        //         if(nums[i]%2==0){
+        //             if(nums[j]%2!=0) count++;
+        //         }
+        //         else{
+        //             if(nums[j]%2==0) count++;
+        //         }
+        //     }
+        //     ans[i]=count;
+        // }
+        // return ans;
     }
 }
