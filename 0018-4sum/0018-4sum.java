3c370b1ba12246c980cbebdc5f345077719c4984
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-11 00:13:53 +0530

    Time: 24 ms (13.59%), Space: 45.7 MB (77.97%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,43 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n=nums.length;
-        HashSet<List<Integer>> ans=new HashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0 ; i<n ; i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
             for(int j=i+1; j<n ; j++){
+                if(j>i+1 && nums[j-1]==nums[j]) continue;
                 int k=j+1,l=n-1;
                 while(k<l){
                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum<target){
+                        while(k<n-1 && nums[k]==nums[k+1]){
+                            k++;
+                        }
                         k++;
                     }
                     else if(sum>target){
+                        while(l>0 && nums[l-1]==nums[l]){
+                            l--;
+                        }
                         l--;
                     }
                     else{
                         ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        k++;
+                        while(k<n-1 && nums[k]==nums[k+1]){
+                            k++;
+                        }
+                        while(l>0 && nums[l-1]==nums[l]){
+                            l--;
+                        }
                         l--;
+                        k++;
                     }
                 }
             }
         }
-        List<List<Integer>> ans1=new ArrayList<>(ans);
-        return ans1;
+        
+        return ans;
     
     }
 }
