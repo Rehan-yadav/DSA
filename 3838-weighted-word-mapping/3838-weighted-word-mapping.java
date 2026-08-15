e74c00fbba7e4bebd1d33f6fee4ff155e43eb52c
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-16 00:38:25 +0530

    Time: 4 ms (27.79%), Space: 46.9 MB (15.32%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        int n=words.length;
+        Character[] alpha=new Character[26];
+        for(int i=0 ; i<26 ; i++){
+            alpha[i]=(char)(122-i);
+        }
+        String ans="";
+        for(int i=0 ; i<n ; i++){
+            String s=words[i];
+            int weig=0;
+            for(int j=0 ; j<s.length();j++){
+                weig+=weights[s.charAt(j)-'a'];
+            }
+            weig=weig%26;
+            ans+=alpha[weig];
+        }
+        
+        return ans;
+    }
+}
