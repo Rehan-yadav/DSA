798852a689d99d66624fe8520aa5f234ab880e95
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-19 00:28:26 +0530

    Time: 25 ms (13.07%), Space: 45.8 MB (47.24%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,43 +1,41 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
         List<List<Integer>> ans=new ArrayList<>();
+        int n=nums.length;
         Arrays.sort(nums);
         for(int i=0 ; i<n ; i++){
-            if(i>0 && nums[i-1]==nums[i]) continue;
-            for(int j=i+1; j<n ; j++){
-                if(j>i+1 && nums[j-1]==nums[j]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1 ; j<n ; j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int k=j+1,l=n-1;
                 while(k<l){
                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum<target){
-                        while(k<n-1 && nums[k]==nums[k+1]){
+                    if(sum==target){
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while(k<n-1 && nums[k+1]==nums[k]){
                             k++;
                         }
                         k++;
-                    }
-                    else if(sum>target){
                         while(l>0 && nums[l-1]==nums[l]){
                             l--;
                         }
                         l--;
+                    }
+                    else if(sum<target){
+                        while(k<n-1 && nums[k+1]==nums[k]){
+                            k++;
+                        }
+                        k++;
                     }
                     else{
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        while(k<n-1 && nums[k]==nums[k+1]){
-                            k++;
-                        }
                         while(l>0 && nums[l-1]==nums[l]){
                             l--;
                         }
                         l--;
-                        k++;
                     }
                 }
             }
         }
-        
         return ans;
-    
     }
 }
