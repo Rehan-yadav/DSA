acef7a4890b1e76166db4634141a433d0cf35a80
Rehan-yadav <rehanyadav574@gmail.com>
2026-08-19 02:05:41 +0530

    Time: 1 ms (100%), Space: 46.2 MB (83.84%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int n=nums.length;
+        int[] freq=new int[501];
+        for(int i=0; i<n ; i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0 ; i<501 ; i++){
+            if(freq[i]%2!=0) return false;
+        }
+        return true;
+    }
+}
