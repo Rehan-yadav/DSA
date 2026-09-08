fe5f8b5bcd00b172abdfe36e98ade66458ea3851
Rehan-yadav <rehanyadav574@gmail.com>
2026-09-08 11:15:29 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,13 +1,7 @@
 class Solution {
     public int countCommas(int n) {
-        int num=n;
-        int dig=0;
-        while(num!=0){
-            dig++;
-            num/=10;
-        }
-        if(dig<=3) return 0;
         int ans=n-999;
-        return ans;
+        if(ans<0) return 0;
+        else return ans;
     }
 }
